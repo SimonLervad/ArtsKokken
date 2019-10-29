819e6d27df7e5ddb8df1bd18a7b41aa936026c2e
Simon Lervad <lervad1996@gmail.com>
2019-10-29 23:20:30 +0100

    kommentar som skal hjælpe med at forstå koden

--- counter.js
+++ counter.js
@@ -1,7 +1,13 @@
+// Tælleren
 let count = 0;
+// Tælleren man ser øverst på skærmen
 var tæller = document.getElementsByClassName("tæller");
+//tælleren man ser nederst på skærmen
 var tællerTo = document.getElementsByClassName("tæller-to");
+// function som tæller op
 var next = function() {
+	// "if" sætningen bestemmer hvorvidt den skal tælle eller ej, altså om man kan komme videre eller er nået til vejs ende.
+	// hvis den er true, og "count++;" vil den gemme alle tal før værden af count. og bagefter fremvise værdien af count.
 	if (count < 4) {
 		count++;
 		console.log(count);
@@ -14,6 +20,7 @@ var next = function() {
 		tæller[i].style.color = "white";
 		tællerTo[count].style.backgroundColor = "green";
 	}
+// looper igennem alle div på siden og gemmer alle undtager den vi nu er nået til.
 	for (j = 0; j < 5; j++) {
 		if (j === count) {
 			$(j).style.display = "block";
@@ -22,7 +29,8 @@ var next = function() {
 		}
 	}
 }
-
+// function som tæller tilbage
+// functionen next minder meget om denne function, den tæller dog bare en ned og looper bagfra i steder fra forfra. 
 var back = function() {
 	if (count > 0) {
 		count--;
